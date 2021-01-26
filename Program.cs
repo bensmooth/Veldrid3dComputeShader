f46228e8dc7d0d3c5ec6c1d65fea14f09be0c979
bensmooth <bensmooth@gmail.com>
2021-01-26 14:29:06 -0600

    Running compute shader.

--- Program.cs
+++ Program.cs
@@ -29,29 +29,21 @@ namespace ComputeShader3d
 
     class Program
     {
-        private static GraphicsDevice CreateDevice(GraphicsBackend backend)
-        {
-            GraphicsDeviceOptions options = new GraphicsDeviceOptions(debug: false);
+        /// <summary>
+        /// The width, height, and depth of the compute texture's output.
+        /// </summary>
+        private const uint OutputTextureSize = 256;
 
-            switch (backend)
-            {
-                case GraphicsBackend.Direct3D11:
-                    return GraphicsDevice.CreateD3D11(options);
-                case GraphicsBackend.Vulkan:
-                    return GraphicsDevice.CreateVulkan(options);
-                case GraphicsBackend.Metal:
-                    return GraphicsDevice.CreateMetal(options);
-                default:
-                    throw new Exception($"Unsupported backend '{backend}'");
-            }
-        }
+        /// <summary>
+        /// The value we're going to fill the texture with.
+        /// </summary>
+        private const float FillValue = 42.42f;
 
 
         static void Main(GraphicsBackend backend)
         {
             using GraphicsDevice graphicsDevice = CreateDevice(backend);
             Console.WriteLine($"Using backend: {graphicsDevice.BackendType}");
-
             ResourceFactory factory = graphicsDevice.ResourceFactory;
 
             byte[] shaderBytes = File.ReadAllBytes("FillComputeShader.comp");
@@ -71,22 +63,58 @@ namespace ComputeShader3d
 
             using Pipeline computePipeline = factory.CreateComputePipeline(ref computePipelineDesc);
 
-            if (FillValueStruct.Size != 16)
-            {
-                throw new Exception($"Expected uniform to be 16 bytes instead of {FillValueStruct.Size}!");
-            }
-
             using DeviceBuffer fillValueBuffer = factory.CreateBuffer(new BufferDescription(FillValueStruct.Size, BufferUsage.UniformBuffer));
-            FillValueStruct fillValue = new FillValueStruct(42.42f);
+
+            // Create our output texture.
+            using Texture computeTargetTexture = factory.CreateTexture(TextureDescription.Texture2D(
+                OutputTextureSize,
+                OutputTextureSize,
+                1,
+                1,
+                PixelFormat.R32_G32_B32_A32_Float,
+                TextureUsage.Sampled | TextureUsage.Storage));
+
+            using TextureView computeTargetTextureView = factory.CreateTextureView(computeTargetTexture);
+
+            using ResourceSet computeResourceSet = factory.CreateResourceSet(new ResourceSetDescription(
+                computeLayout,
+                computeTargetTextureView,
+                fillValueBuffer));
 
             using CommandList cl = factory.CreateCommandList();
             cl.Begin();
-            cl.UpdateBuffer(fillValueBuffer, 0, fillValue);
 
-            // TODO: Actually use our compute shader to fill the texture.
+            cl.UpdateBuffer(fillValueBuffer, 0, new FillValueStruct(FillValue));
+
+            // Actually use our compute shader to fill the texture.
             cl.SetPipeline(computePipeline);
+            cl.SetComputeResourceSet(0, computeResourceSet);
+            const uint GroupDivisorXY = 16;
+            cl.Dispatch(OutputTextureSize / GroupDivisorXY, OutputTextureSize / GroupDivisorXY, groupCountZ: 1);
 
             cl.End();
+            graphicsDevice.SubmitCommands(cl);
+            graphicsDevice.WaitForIdle();
+
+            // TODO: Read back from our texture and make sure it has been properly filled.
+        }
+
+
+        private static GraphicsDevice CreateDevice(GraphicsBackend backend)
+        {
+            GraphicsDeviceOptions options = new GraphicsDeviceOptions(debug: false);
+
+            switch (backend)
+            {
+                case GraphicsBackend.Direct3D11:
+                    return GraphicsDevice.CreateD3D11(options);
+                case GraphicsBackend.Vulkan:
+                    return GraphicsDevice.CreateVulkan(options);
+                case GraphicsBackend.Metal:
+                    return GraphicsDevice.CreateMetal(options);
+                default:
+                    throw new Exception($"Unsupported backend '{backend}'");
+            }
         }
     }
 }
